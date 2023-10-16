b8c01f48586fdbc4f083268ea1b16efc6b772de1
Michael Omeiza Suleiman <mosuleiman19@gmail.com>
2023-10-16 22:20:40 +0400

    Working with Jdbc to authenticate a user.

---
+++ jdbc-security/README.md
@@ -0,0 +1,4 @@
+# JDBC Security
+
+The idea here is the setup basic in memory username and password user authentication.
+Starting with the 'older way' of configuring spring security to the newer use of SecurityFilterChain and the Lambda DSL.
---
+++ jdbc-security/src/main/java/dev/codingstoic/jdbcsecurity/JdbcSecurityApplication.java
@@ -0,0 +1,13 @@
+package dev.codingstoic.jdbcsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcSecurityApplication.class, args);
+	}
+
+}
